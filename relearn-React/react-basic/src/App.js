a28d8557999158dfa0304e817726001c21e67a1c
yanglu <v.yanglu@shengqugames.com>
2024-05-30 16:36:00 +0800

    day2

--- relearn-React/react-basic/src/App.js
+++ relearn-React/react-basic/src/App.js
@@ -1,17 +1,20 @@
-// 样式导入
-import "./index.css";
-const style = {
-  color: "red",
-  fontSize: "120%",
-};
+import { useState } from "react";
+// 手控绑定表单
+// 1、声明一个React状态 -useState
+
+// 2、核心绑定流程
+// 1.通过value属性绑定React状态
+// 2.绑定onChange事件 通过时间参数e拿到输入框最新的值 反向修改到React状态
+
 function App() {
+  const [value, setValue] = useState("");
   return (
     <div className="App">
-      {/* 行内样式 */}
-      <span style={{ color: "red", fontSize: "120%" }}>this is span</span>
-      <span style={style}>this is span</span>
-      {/* class类名控制 */}
-      <span className="foo">this a class span</span>
+      <input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        type="text"
+      ></input>
     </div>
   );
 }
