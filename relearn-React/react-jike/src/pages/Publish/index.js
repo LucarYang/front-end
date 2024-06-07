a30d86d05fdc53f53a4415ec4d8470f374a7b4bd
yanglu <v.yanglu@shengqugames.com>
2024-06-07 14:43:00 +0800

    富文本编辑器

--- relearn-React/react-jike/src/pages/Publish/index.js
+++ relearn-React/react-jike/src/pages/Publish/index.js
@@ -13,6 +13,9 @@ import { PlusOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import "./index.scss";
 
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
 const { Option } = Select;
 
 const Publish = () => {
@@ -29,8 +32,8 @@ const Publish = () => {
         }
       >
         <Form
-          labelCol={{ span: 4 }}
-          wrapperCol={{ span: 16 }}
+          labelCol={{ span: 2 }}
+          wrapperCol={{ span: 10 }}
           initialValues={{ type: 1 }}
         >
           <Form.Item
@@ -53,7 +56,13 @@ const Publish = () => {
             label="内容"
             name="content"
             rules={[{ required: true, message: "请输入文章内容" }]}
-          ></Form.Item>
+          >
+            <ReactQuill
+              className="publish-quill"
+              theme="snow"
+              placeholder="请输入内容"
+            />
+          </Form.Item>
 
           <Form.Item wrapperCol={{ offset: 4 }}>
             <Space>
